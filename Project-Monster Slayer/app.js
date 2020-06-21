978f704e66fb6a02e12a452884ed0b517c333585
LDY681 <ldydyx@gmail.com>
2020-06-21 23:03:55 +0800

    finish up

--- Project-Monster Slayer/app.js
+++ Project-Monster Slayer/app.js
@@ -13,24 +13,41 @@ var vm = new Vue({
             this.playerHealth = 100;
             this.monsterHealth = 100;
             this.specAttackAvailable = false;
+            this.gameLog = [];
         },
         attack: function(){
             let monsterDamage = getRandomInRange(0,20);
             this.playerHealth -= monsterDamage;
-            this.gameLog.unshift("[怪兽攻击]玩家受到怪兽的" + monsterDamage + "点伤害");
+            this.gameLog.unshift({
+                message: "[怪兽攻击]玩家受到怪兽的" + monsterDamage + "点伤害", 
+                type: 'monsterAttack',
+                color: 'red',
+            });
             let playerDamage = getRandomInRange(0,25);
             this.monsterHealth -= playerDamage;
-            this.gameLog.unshift("[普通攻击]玩家对怪兽造成" + playerDamage + "点伤害");
+            this.gameLog.unshift({
+                message: "[普通攻击]玩家对怪兽造成" + playerDamage + "点伤害", 
+                type: 'playerAttack',
+                color: 'brown',
+            });
             this.specAttackAvailable = Math.round(Math.random())?false:true;
         },
         specAttack: function(){
             if (this.specAttackAvailable === true){
                 let monsterDamage = getRandomInRange(0,20);
                 this.playerHealth -= monsterDamage;
-                this.gameLog.unshift("[怪兽攻击]玩家受到怪兽的" + monsterDamage + "点伤害");
+                this.gameLog.unshift({
+                    message: "[怪兽攻击]玩家受到怪兽的" + monsterDamage + "点伤害", 
+                    type: 'monsterAttack',
+                    color: 'red',
+                });
                 let playerDamage = getRandomInRange(0,50);
                 this.monsterHealth -= playerDamage;
-                this.gameLog.unshift("[特殊攻击]玩家对怪兽造成" + playerDamage + "点伤害");
+                this.gameLog.unshift({
+                    message: "[特殊攻击]玩家对怪兽造成" + playerDamage + "点伤害", 
+                    type: 'playerSpecAttack',
+                    color: 'purple',
+                });
                 this.specAttackAvailable = Math.round(Math.random())?false:true;
             }else{
                 alert("您当前无法使用特殊攻击!");
@@ -39,10 +56,18 @@ var vm = new Vue({
         heal: function(){
             let playerHeal = getRandomInRange(0,50);
             this.playerHealth += playerHeal;
-            this.gameLog.unshift("[恢复]玩家恢复" + playerHeal + "点体力");
+            this.gameLog.unshift({
+                message: "[恢复]玩家恢复" + playerHeal + "点体力", 
+                type: 'playerHeal',
+                color: 'green',
+            });
             let monsterDamage = getRandomInRange(0,20);
             this.playerHealth -= monsterDamage;
-            this.gameLog.unshift("[怪兽攻击]玩家受到怪兽的" + monsterDamage + "点伤害");
+            this.gameLog.unshift({
+                message: "[怪兽攻击]玩家受到怪兽的" + monsterDamage + "点伤害", 
+                type: 'monsterAttack',
+                color: 'red',
+            });
             if (this.playerHealth > 100){
                 this.playerHealth = 100;
             }
@@ -55,6 +80,7 @@ var vm = new Vue({
                     this.startGame();
                 }else{
                     this.gameState = false;
+                    this.gameLog = [];
                 }
             }
         },
@@ -64,6 +90,7 @@ var vm = new Vue({
                     this.startGame();
                 }else{
                     this.gameState = false;
+                    this.gameLog = [];
                 }
             }
         }
